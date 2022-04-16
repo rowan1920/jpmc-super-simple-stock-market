96b55fdf4a20c32a3dc04c15575c4ee12952fe53
Rowan <rowanoreilly12@gmail.com>
2022-04-16 10:13:18 +0100

    Created stock model

---
+++ models/stock.py
@@ -0,0 +1,53 @@
+import re
+from typing import Optional
+from pydantic import BaseModel, validator
+
+
+VALID_STOCK_SYMBOL_EXPRESSION: str = r'^[A-Z]{3}$'
+VALID_STOCK_TYPE_EXPRESSION: str = r'^(preferred|common)$'
+
+
+class Stock(BaseModel):
+    """
+    Args:
+        symbol (str): 3 letter stock symbol.
+        type (str): Either `preferred` or `common`
+        last_dividend (int): Value of the last dividend in pence
+        fixed_dividend (float): An optional fixed_dividend rate
+        par_value (int): Par value in pence
+    """
+    symbol: str
+    type: str
+    last_dividend: int
+    fixed_dividend: Optional[float]
+    par_value: int
+
+    @validator('symbol')
+    def check_symbol(cls, v: str) -> str:
+        if not re.match(VALID_STOCK_SYMBOL_EXPRESSION, v):
+            raise ValueError('Stock symbol doesn\'t match the regex "{}"'.format(VALID_STOCK_SYMBOL_EXPRESSION))
+        return v
+
+    @validator('type')
+    def check_type(cls, v: str) -> str:
+        if not re.match(VALID_STOCK_TYPE_EXPRESSION, v):
+            raise ValueError('Stock type doesn\'t match the regex "{}"'.format(VALID_STOCK_TYPE_EXPRESSION))
+        return v
+
+    @validator('last_dividend')
+    def check_last_dividend(cls, v: int) -> int:
+        if v < 0:
+            raise ValueError('Stock last_dividend must be positive or zero')
+        return v
+
+    @validator('fixed_dividend')
+    def check_fixed_dividend(cls, v: Optional[float]) -> Optional[float]:
+        if (v is not None) and (v < 0):
+            raise ValueError('Stock {}: fixed_dividend must be positive, zero or not set'.format(v))
+        return v
+
+    @validator('par_value')
+    def check_par_value(cls, v: int) -> int:
+        if v <= 0:
+            raise ValueError('Stock par_value must be greater than zero')
+        return v
