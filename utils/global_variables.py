dbf3d771c838dad40b88ca56585b49568251e28a
Rowan <rowanoreilly12@gmail.com>
2022-04-17 18:42:10 +0100

    Added trades to the global_variables

--- utils/global_variables.py
+++ utils/global_variables.py
@@ -1,8 +1,9 @@
 """
 This module is used in place of a database to store shared data globally.
 """
-from typing import Dict
+from typing import Dict, List
 
-from models import Stock
+from models import Stock, Trade
 
+trades: List[Trade] = []
 stocks: Dict[str, Stock] = {}
