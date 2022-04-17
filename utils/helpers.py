ee8ea64059220bbc80c1819b6e13c02e6b586e0c
Rowan <rowanoreilly12@gmail.com>
2022-04-17 18:53:53 +0100

    Added volume_weighted_stock_price support
    Created the calculate_volume_weighted_stock_price helper

--- utils/helpers.py
+++ utils/helpers.py
@@ -15,6 +15,34 @@ def record_trade(stock: Stock, quantity: int, trade_type: str, price: int) -> No
         traded_price=price
     )))
 
+
+def calculate_volume_weighted_stock_price(stock_symbol: str) -> float:
+    """
+    Due to the structure of the trade data in memory, this method would need to loop through all stocks in the
+    last 15 minutes. This would not be optimal in a production system with large amounts of data.
+    """
+    total_product_of_quantity_and_price: float = 0
+    total_traded_quantity: float = 0
+    time_15_minutes_ago: datetime = datetime.now() - timedelta(minutes=15)
+
+    for trade in global_variables.trades:
+        if trade.timestamp <= time_15_minutes_ago:
+            break
+        if trade.stock.symbol == stock_symbol:
+            total_product_of_quantity_and_price += trade.traded_price * trade.quantity
+            total_traded_quantity += trade.quantity
+
+    if total_product_of_quantity_and_price == 0 or total_traded_quantity == 0:
+        raise ValueError('There have been no trades for {} in the past 15 minutes'.format(stock_symbol))
+
+    try:
+        to_return = total_product_of_quantity_and_price / total_traded_quantity
+    except ZeroDivisionError:
+        to_return = 0
+
+    return to_return
+
+
 def hydrate_stocks(data) -> Dict[str, Stock]:
     hydrated_stocks: Dict[str, Stock] = {}
     for stock_data in data:
