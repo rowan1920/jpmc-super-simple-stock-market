d3fc8946b1c4135211fe6d50b68eb264ebf8bc35
Rowan <rowanoreilly12@gmail.com>
2022-04-17 18:41:32 +0100

    Created a record_trade helper method

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,5 +1,19 @@
+from datetime import datetime, timedelta
 from typing import Dict
 
+from models import Trade, Stock
+from utils import global_variables
+
+
+def record_trade(stock: Stock, quantity: int, trade_type: str, price: int) -> None:
+    current_timestamp = datetime.now()
+    global_variables.trades.insert(0, (Trade(
+        stock=stock,
+        timestamp=current_timestamp,
+        quantity=quantity,
+        type=trade_type,
+        traded_price=price
+    )))
 
 def hydrate_stocks(data) -> Dict[str, Stock]:
     hydrated_stocks: Dict[str, Stock] = {}
